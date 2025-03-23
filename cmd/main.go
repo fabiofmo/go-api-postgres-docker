4c5aba59a5d68fbd723853aff7ff4127d929a780
fabiofmo <fabiofmo@hotmail.com>
2025-03-23 15:44:07 -0300

    feat: product model & controller

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/fabiofmo/go-api-postgres-docker/controller"
+	"github.com/fabiofmo/go-api-postgres-docker/model"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	router := gin.Default()
+
+	ProductController := controller.NewProductController()
+	router.GET("/products", ProductController.GetProducts)
 
 	router.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
@@ -12,6 +21,18 @@ func main() {
 		})
 	})
 
+	router.GET("/product", getAllProducts)
 	router.Run("localhost:8080")
 
+}
+
+func getAllProducts(context *gin.Context) {
+	products := []model.Product{
+		{
+			ID:    1,
+			Name:  "Batata frita",
+			Price: 20,
+		},
+	}
+	context.IndentedJSON(http.StatusOK, products)
 }
